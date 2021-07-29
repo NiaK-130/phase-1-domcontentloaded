b7d39e647ef7d3d296d80c50146239c452a23db1
Nia K <nsjkappen@gmail.com>
2021-07-29 16:20:33 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    changeText();
+  }); 
+
+
+  function changeText(text){
+      let change = document.querySelector('#text')
+      change.textContent = "This is really cool!"
+  }
+
+
+  //<body>
+//   <p id="text">JavaScript is so cool. It lets me add text to my page programmatically.</p>
+//   <script type="text/javascript" src="index.js"></script>
+// </body>
+// </html>
